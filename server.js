7d56c0eebd251252247cedcf35532e915788314f
Molly Kent <kent.molly@gmail.com>
2015-09-09 09:17:49 -0700

    Close #10 Add 404 page and fine tune server behavior

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.get('/index.html', function(req, res) {//translates to the URL localhost/300
   res.status(200).sendFile(__dirname + '/public/index.html');
   //only call ONE .send
 });
+app.get('/index', function(req, res) {//translates to the URL localhost/3000/greet
+  //req is request and res is response
+  res.status(200).sendFile(__dirname + '/public/index.html');
+  //only call ONE .send
+});
 
-// app.get('/*', function(req, res) {
-  // res.status(404).sendFile(__dirname + '/index.html');
-// });
+app.get('/*', function(req, res) {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(app.get('port'), function(){ //environmental variable process.env pulls the port from the command line, if it doesn't exist it will go to 3000.  in the command line we can now say "PORT=5000 node server" and the server will pick up on the change.  SWEET
 
