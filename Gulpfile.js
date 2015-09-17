a12d0173d9603a4115c30c49c931745dd4c7f4c5
Molly Kent <kent.molly@gmail.com>
2015-09-17 14:48:08 -0700

    Make copy of sass directory to refactor structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function() {
-  gulp.src("public/sass/**/*.scss")
+  gulp.src("public/sass-copy/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./public/css"));
 });
 
 gulp.task("watch", ["sass"], function() {
-  gulp.watch("public/sass/**/*.scss", ["sass"]);
+  gulp.watch("public/sass-copy/**/*.scss", ["sass"]);
 });
