be5d6475479a30f5dce88ae8e62029da617828fe
Molly Kent <kent.molly@gmail.com>
2015-09-17 13:27:39 -0700

    Add carousel functionality to Clients, refactor _slider.scss

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,13 +1,23 @@
 // from http://unslider.com/
 'use strict';
 $(function() {
-  $('.carousel').unslider({
-  speed: 500,              //  The speed to animate each slide
-  delay: 3000,             //  The delay between slide animations
-  keys: true,              //  Enable keyboard arrow shortcuts
-  dots: false             //  Display dot navigation - done manually below
-});
-  var slidey = $('.carousel').unslider(),
+  $('.img-carousel').unslider({
+    speed: 500,              //  The speed to animate each slide
+    delay: 3000,             //  The delay between slide animations
+    keys: true,              //  Enable keyboard arrow shortcuts
+    dots: false             //  Display dot navigation - done manually below
+  });
+  var slidey = $('.img-carousel').unslider(),
     data = slidey.data('unslider');
+    data.dots();
+
+  $('.fig-carousel').unslider({
+    speed: 500,              //  The speed to animate each slide
+    delay: 3000,             //  The delay between slide animations
+    keys: true,              //  Enable keyboard arrow shortcuts
+    dots: false             //  Display dot navigation - done manually below
+  });
+  var slidey2 = $('.fig-carousel').unslider(),
+    data = slidey2.data('unslider');
     data.dots();
 });
--- public/js/smooth_scroll.js
+++ public/js/smooth_scroll.js
@@ -1,6 +1,26 @@
 'use strict';
 
-$('a[href^="#"]').on('click', function(e) {
+$('.flex-navbar a').on('click', function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  var $target = $(target);
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 1600, 'swing', function () {
+      window.location.hash = target;
+  });
+});
+$('.signup a').on('click', function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  var $target = $(target);
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 1600, 'swing', function () {
+      window.location.hash = target;
+  });
+});
+$('.mock-button').on('click', function(e) {
   e.preventDefault();
   var target = this.hash;
   var $target = $(target);
