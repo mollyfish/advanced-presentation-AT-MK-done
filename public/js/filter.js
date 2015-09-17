f4d2e2f955bf82799772c87b6072ebf12c3a390f
Alina To <alina.to@gmail.com>
2015-09-17 14:55:23 -0700

    Add effects to fadeIn and fadeOut filtered gallery items

---
+++ public/js/filter.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$('[data-show-tag]').click(function(event) {
+  event.preventDefault();
+
+  var tag = $(event.currentTarget).data('show-tag');
+
+  console.log(tag);
+
+  if(tag == 'all') {
+    $('[data-tags]').fadeIn();
+    return;
+  }
+
+  $('[data-tags]').fadeOut();
+
+  $('[data-tags*="' + tag + '"]').fadeIn();
+});
